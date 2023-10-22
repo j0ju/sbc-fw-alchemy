e6d76a09d16dca9402d43360a02506dbb7b596a9
Joerg Jungermann <joerg.jungermann@googlemail.com>
2023-10-22 23:51:33 +0200

    armbian-bookworm-vanilla: initial add

---
+++ armbian-bookworm-vanilla.Dockerfile.d/100_import_armbian.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+# (C) 2023 Joerg Jungermann, GPLv2 see LICENSE
+
+PS4='> ${0##*/}: '
+#set -x
+
+mv /target /target.old
+mv /target.old/part1 /target
+mv /target.old/uboot.egn /target/boot/uboot.egn
+rm -rf /target.old
+tar cf - -C /dev . | tar xf - -C /target/dev
