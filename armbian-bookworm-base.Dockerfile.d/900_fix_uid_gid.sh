68f141936bfbd991b83d66727e3fffa7f30baf2d
Joerg Jungermann <joerg.jungermann@googlemail.com>
2023-11-19 02:39:36 +0100

    armbian-bookworm-base.Dockerfile.d/900_fix_uid_gid.sh: move UIDs and GIDs to freeup 900er range

---
+++ armbian-bookworm-base.Dockerfile.d/900_fix_uid_gid.sh
@@ -0,0 +1,82 @@
+#!/bin/sh -eu
+# (C) 2023 Joerg Jungermann, GPLv2 see LICENSE
+
+PS4='> ${0##*/}: '
+#set -x
+
+UIDS="995 996 997 998 999"
+GIDS="995 996 997 998 999"
+
+next_free_uid() {
+  local fresh_uid="${1:-200}"
+  while :; do
+    < /target/etc/passwd awk -F':' '$3 == "'"$fresh_uid"'"' | grep ^ > /dev/null || \
+    break
+    fresh_uid=$((fresh_uid+1))
+  done
+  echo "$fresh_uid"
+}
+
+next_free_gid() {
+  local fresh_gid="${1:-200}"
+  while :; do
+    < /target/etc/group awk -F':' '$3 == "'"$fresh_gid"'"' | grep ^ > /dev/null || \
+    break
+    fresh_gid=$((fresh_gid+1))
+  done
+  echo "$fresh_gid"
+}
+
+is_uid_used() {
+  local uid="${1:-0}"
+  < /target/etc/passwd awk -F':' '$3 == "'"$uid"'"' || return $?
+}
+
+is_gid_used() {
+  local gid="${1:-0}"
+  < /target/etc/group awk -F':' '$3 == "'"$gid"'"' || return $?
+}
+
+#- reassign gids
+for gid in $GIDS; do
+  #- check if GID is qccupied
+  is_gid_used $gid || continue
+  #- get free GID
+  fresh_gid="$(next_free_gid)"
+
+  #- change GID /etc/group
+  awk -F: '/^/ { OFS=":"; if ( $3=='"$gid"' ) $3='"$fresh_gid"'; print $0 }' < /target/etc/group > /target/etc/group-
+  cat /target/etc/group- > /target/etc/group
+
+  # change GID /etc/passwd
+  awk -F: '/^/ { OFS=":"; if ( $4=='"$gid"' ) $4='"$fresh_gid"'; print $0 }' < /target/etc/passwd > /target/etc/passwd-
+  cat /target/etc/passwd- > /target/etc/passwd
+
+  # change filesystem permission
+  find /target -xdev -gid $gid -exec chgrp $fresh_gid {} +
+done
+
+#- reassing uids
+for uid in $UIDS; do
+  #- check if GID is qccupied
+  is_uid_used $uid || continue
+
+  #- get free GID
+  fresh_uid="$(next_free_uid)"
+
+  # change UID /etc/passwd
+  awk -F: '/^/ { OFS=":"; if ( $3=='"$uid"' ) $3='"$fresh_uid"'; print $0 }' < /target/etc/passwd > /target/etc/passwd-
+  cat /target/etc/passwd- > /target/etc/passwd
+
+  # change filesystem permission
+  find /target -xdev -gid $gid -exec chown $fresh_gid {} +
+done
+
+#- cleanup any workfiles
+rm /target/etc/passwd-
+rm /target/etc/group-
+
+sed -i -e s/,,,// /etc/passwd
+pwck -s
+grpck -s
+
