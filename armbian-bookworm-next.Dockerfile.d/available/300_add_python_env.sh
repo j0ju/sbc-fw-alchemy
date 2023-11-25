07eef103dace4a4b1a92c6148a577eccbd63b154
Joerg Jungermann <joerg.jungermann@googlemail.com>
2023-11-25 21:23:31 +0100

    armbian-bookworm-next: add some text collectors

--- armbian-bookworm-next.Dockerfile.d/available/300_add_python_env.sh
+++ armbian-bookworm-next.Dockerfile.d/available/300_add_python_env.sh
@@ -9,3 +9,7 @@ chroot /target apt-get install -y \
   python3-pip \
   python3-smbus python3-smbus2
 
+umask 022
+
+chroot /target pip3 install --break-system-packages -U pip
+chroot /target pip3 install --break-system-packages    RPi.bme280
