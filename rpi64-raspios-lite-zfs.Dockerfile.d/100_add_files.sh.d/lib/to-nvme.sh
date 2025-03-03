754f2752c9ab62c675cb14cb1b5fd577bb9defc4
abra <abra@tpn3>
2025-03-03 19:02:21 +0000

    rpi64-raspios-lite-zfs: based on rpi64-raspios-lite-base prepared for root on ZFS

---
+++ rpi64-raspios-lite-zfs.Dockerfile.d/100_add_files.sh.d/lib/to-nvme.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -x
+if [ $$ = 1 ]; then
+  mount
+  mount -t tmpfs tmpfs /var/tmp
+  mount -t tmpfs tmpfs /tmp
+  mount -w /boot/firmware
+  /lib/systemd/systemd-udevd &
+fi
+sh /lib/rootfs-to.sh /dev/nvme0n1
+sync
+
+if [ $$ = 1 ]; then
+  umount /boot/firmware
+  echo s > /proc/sysrq-trigger
+  sleep 3
+  echo u > /proc/sysrq-trigger
+  sleep 3
+  echo b > /proc/sysrq-trigger
+fi
