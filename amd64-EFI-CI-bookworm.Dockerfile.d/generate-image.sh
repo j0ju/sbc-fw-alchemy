44bca981707bec729e1ef483acd8e8859df6c9c2
j0ju <j0ju@mac>
2025-04-10 13:51:39 +0000

    amd64-EFI-CI-bookworm: add proper grub install and fstab adaption

--- amd64-EFI-CI-bookworm.Dockerfile.d/generate-image.sh
+++ amd64-EFI-CI-bookworm.Dockerfile.d/generate-image.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
 set -eu
-#set -x
+umask 022
 
 #---
 EFI_SIZE_k=102400
 MIN_FREE_k=524288
+FS=ext4
 
 #--- calculate minimal image size based on EFI_SIZE_k and MIN_FREE_k
 #    MIN_FREE_k is the minimal amount of space free in the rootfs
@@ -49,27 +50,60 @@ cleanup() {
 trap cleanup EXIT TERM HUP INT USR1 USR2
 DEVS="$(kpartx -av "$IMAGE" | grep -oE 'loop[0-9]p[^ ]+' | sort -u)"
 
-P1="$(echo $DEVS | grep -E -o "loop[0-9]p1")"
-P4="$(echo $DEVS | grep -E -o "loop[0-9]p4")"
+ROOT_DEV="/dev/mapper/$(echo $DEVS | grep -E -o "loop[0-9]p1")"
+EFI_DEV="/dev/mapper/$(echo $DEVS | grep -E -o "loop[0-9]p4")"
 mkdir -p /mnt/
 
 echo "MKFS"
-mkfs.ext4 -L rootfs -q /dev/mapper/$P1
-mkfs.vfat -n EFI /dev/mapper/$P4
+case "$FS" in
+  ext[234] ) mkfs.ext4 "$ROOT_DEV" ;;
+esac
+mkfs.vfat -n EFI "$EFI_DEV"
 
-mount -t ext4 /dev/mapper/$P1 /mnt
+mount "$ROOT_DEV" /mnt
 mkdir -p /mnt/boot/efi
-mount -t vfat /dev/mapper/$P4 /mnt/boot/efi
+mount -t vfat "$EFI_DEV" /mnt/boot/efi
 
 #--- copy rootfs
 echo "COPY"
 tar cf - --numeric-owner --acls --xattrs -C /target . | tar xf - -C /mnt --numeric-owner --acls --xattrs
+rm -rf /target/sys /target/proc /target/tmp /target/run /target/var/tmp
+mkdir -p /target/sys /target/proc /target/tmp /target/run /target/var/tmp
+chmod 1777 /target/tmp /target/var/tmp
+
+#--- ensure needed environment for bootloader installation
+mount --bind /sys /mnt/sys
+mount --bind /proc /mnt/proc
+mount --bind /dev /mnt/dev
+mount --bind /tmp /mnt/tmp
+mount --bind /tmp /mnt/var/tmp
+
+# we are in an CRI and /dev/disk/by-uuid /dev/disk/by-partuuid might not be avail
+# --> create it for out devs
+mkdir -p /dev/disk/by-uuid /dev/disk/by-partuuid
+for d in "$ROOT_DEV" "$EFI_DEV"; do
+  ( eval "$(blkid -o export -p $d)"
+    ln -s $d /dev/disk/by-uuid/$UUID
+    ln -s $d /dev/disk/by-partuuid/$PART_ENTRY_UUID
+  )
+done
 
 #--- install EFI grub
-dpkg --add-architecture amd64
-apt-get update
-apt-get install -y grub-efi-amd64-bin/stable grub-efi-arm64-bin/stable  grub-efi/stable
-grub-install --target x86_64-efi --efi-directory=/mnt/boot/efi --removable --boot-directory=/mnt/boot /dev/loop0
+BOOT_DEV="${ROOT_DEV%p1}"
+grub-install --target x86_64-efi --efi-directory=/mnt/boot/efi --removable --boot-directory=/mnt/boot "${BOOT_DEV}"
+chroot /mnt update-grub
+
+#--- adapt fstab and machine-id
+ROOT_UUID="$(blkid -o value -s UUID "$ROOT_DEV")"
+EFI_UUID="$(blkid -o value -s UUID "$EFI_DEV")"
+# /
+( FS=$FS MNT=/ BLKDEV="UUID=$ROOT_UUID"
+  sed -i -r -e 's!^([^[:space:]]+)[[:space:]]+('"$MNT"')[[:space:]]+[^[:space:]]+!'"$BLKDEV"' \2 '"$FS"'!' "/mnt/etc/fstab"
+)
+# /boot/efi
+( FS=vfat MNT=/boot/efi BLKDEV="UUID=$EFI_UUID"
+  sed -i -r -e 's!^([^[:space:]]+)[[:space:]]+('"$MNT"')[[:space:]]+[^[:space:]]+!'"$BLKDEV"' \2 '"$FS"'!' "/mnt/etc/fstab"
+)
 
 [ -z "$OWNER" ] || \
   chown "$OWNER${GROUP:+:$GROUP}" "$IMAGE"
