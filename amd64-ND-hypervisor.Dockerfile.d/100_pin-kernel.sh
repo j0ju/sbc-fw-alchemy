50f9c627554d8d1044f8ce13552f53584d049bbc
j0ju <j0ju@mac>
2025-04-16 15:10:09 +0200

    amd64-ND-hypervisor: pin kernel to debian version 6.1.0-18

--- amd64-ND-hypervisor.Dockerfile.d/100_pin-kernel.sh
+++ amd64-ND-hypervisor.Dockerfile.d/100_pin-kernel.sh
@@ -1,11 +1,34 @@
 #!/bin/sh -eu
+PINs="linux-image-6.1.0-18-amd64 linux-headers-6.1.0-18-amd64"
 
-#set -x
+#- files in $0.d will be pupolualated to rootfs in /target
 FSDIR="$0.d"
 if [ -d "$FSDIR" ]; then
-  . "$SRC/add-files.sh"
+  . "/src/img-mangler.Dockerfile.d/100_add_files.sh"
 else
   . "$SRC/lib.sh"; init
 fi
 
-# TODO: install pinned kernel
+# remove all other
+TO_PURGE=
+for kpkg in $(chroot /target dpkg -l linux-image*  linux-headers*| awk '$1 =="ii" && $0=$2'); do
+  for p in $PINs; do
+    echo "$kpkg ~ ${p%-*}"
+    case "${kpkg}" in
+      "${p%-*}"* )
+        p=
+        break
+    esac
+  done
+  [ -z "$p" ] || TO_PURGE="$TO_PURGE $kpkg"
+done
+
+# pinning is done via uninstalling meta packages linux-image-amd64 and linux-headers-amd64, 
+# so no auto updates
+chroot /target dpkg -P $TO_PURGE
+
+# install $PINs
+chroot /target apt-get update
+chroot /target apt-get install -y $PINs
+
+# remove all other
