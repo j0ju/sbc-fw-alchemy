b3ab162606e0bbebb5a7868ca61810653f1cd7ad
j0ju <j0ju@mac>
2025-04-25 23:20:30 +0200

    amd64-ND-hypervisor: install DOCA/OFED requisites and download .debs to /srv/ci-installer/debs for installtion during install

--- amd64-ND-hypervisor.Dockerfile.d/200_add_OFED.sh
+++ amd64-ND-hypervisor.Dockerfile.d/200_add_OFED.sh
@@ -9,8 +9,18 @@ else
 fi
 
 set -x
+chroot /target apt-get install -y \
+  build-essential dkms \
+  #
+# FIXME: merge this with no after install 
+
+chroot /target apt-get clean
 chroot /target apt-get update
 chroot /target apt-get install -yd \
   mlnx-ofed-kernel-utils mlnx-tools \
   mlnx-nvme-dkms mlnx-ofed-kernel-dkms \
   #
+
+mkdir -p /target/srv/ci-installer/debs
+cp /target/var/cache/apt/archives/*.deb /target/srv/ci-installer/debs
+
