1c89544f9b5e5068dc95785224b53c47aead869f
root <root@>
2025-06-01 20:19:01 +0000

    turingpi2-bmc-git.Workspace.d: rename workspace from turingpi2-bmc-git

---
+++ turingpi2-buildroot.Workspace.d/prepare.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu
+umask 022
+set -x
+
+#- as I am running as UID=0 in container
+export FORCE_UNSAFE_CONFIGURE=1
+
+cd /workspace
+
+if [ -d bmc-firmware ]; then
+  cd bmc-firmware/buildroot
+  make
+  #echo "E: already checked out, clean manually or do make clean-volumes"
+  #exit 1
+else :
+  git clone https://github.com/turing-machines/BMC-Firmware.git bmc-firmware
+  cd bmc-firmware
+
+  #- patch kernel config
+  cp "${0%/*}"/linux_defconfig  tp2bmc/board/tp2bmc/linux_defconfig
+
+  #- prepare /workspace/bmc-firmware/buildroot
+  ./scripts/configure.sh
+
+  #- link image output to ./output
+  mkdir -p /src/output/turingpi2-buildroot
+  mkdir -p /workspace/bmc-firmware/buildroot/output
+  rm -rf /workspace/bmc-firmware/buildroot/output/images
+  ln -s /src/output/turingpi2-buildroot /workspace/bmc-firmware/buildroot/output/images
+
+  ./scripts/build.sh
+fi
+
+[ -z "$OWNER" ] || \
+  chown -R "$OWNER${GROUP:+:$GROUP}" /workspace/bmc-firmware/buildroot/output/images/.
