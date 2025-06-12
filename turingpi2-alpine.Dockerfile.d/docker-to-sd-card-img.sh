0a2cfc3726ddaac1cbfcf18e8088bb5e66200e86
j0ju <jj@borkum.net>
2025-06-12 17:36:18 +0200

    turingpi2-alpine: add make target and scripts to generate SD card image file

---
+++ turingpi2-alpine.Dockerfile.d/docker-to-sd-card-img.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+# (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
+set -eu
+
+#---
+MIN_FREE_MB=${MIN_FREE_MB:-64}
+
+#--- calculate image size
+USAGE_KB="$(du -sk /target | { read kb _; echo $kb; })"
+IMAGE_SIZE_KB=$(( USAGE_KB + MIN_FREE_MB*1024 ))
+
+IMAGE="$2"
+SRC="$1"
+
+#--- safe cleanup
+cleanup() {
+  local rs=$?
+  local d
+  if [ $rs = 0 ]; then
+    [ -z "$OWNER" ] || \
+      chown "$OWNER${GROUP:+:$GROUP}" "$IMAGE"
+  else
+    rm -f "$IMAGE"
+  fi
+  cd /
+  for m in /mnt/part* /mnt; do
+    umount "$m" 2> /dev/null || :
+  done
+  for d in ${DEVS:-}; do
+    if [ -b "/dev/mapper/$d" ]; then
+      dmsetup remove "/dev/mapper/$d" 2> /dev/null || :
+    fi
+    if [ -b /dev/"${d%p*}" ]; then
+      losetup -d "/dev/${d%p*}" 2> /dev/null || :
+    fi
+    d="${d%p*}"
+    if [ -b "/dev/loop/${d#*loop}" ]; then
+      losetup -d "/dev/loop/${d#*loop}" 2> /dev/null || :
+    fi
+  done
+  trap '' EXIT
+  exit $rs
+}
+trap cleanup EXIT TERM HUP INT USR1 USR2
+
+echo "EMPTY $IMAGE"
+#--- generate sparse image
+: > $IMAGE
+dd if=/dev/zero bs=1024 count=0 seek=$IMAGE_SIZE_KB of=$IMAGE status=none
+#--- partition it
+
+echo "FDISK $IMAGE"
+# 1st part will only be as large as the current image size
+sfdisk $IMAGE > /dev/null <<EOF
+  label: dos
+  1: type=83 start=2048 bootable
+EOF
+
+DEVS="$(kpartx -av "$IMAGE" | grep -oE 'loop[^ ]+' | sort -u)"
+
+P1="$(echo $DEVS | grep -E -o "[^ ]+p1")"
+mkdir -p /mnt/
+
+echo "MKFS $IMAGE"
+mkfs.ext4 -q -L tpi2-alpine /dev/mapper/$P1
+mount -t ext4 /dev/mapper/$P1 /mnt
+
+echo "COPY $IMAGE <- $SRC"
+tar xf "$SRC" -C /mnt --atime-preserve
+
+#--- write uboot for sdcard boot
+echo "UBOOT $IMAGE"
+dd if=/mnt/boot/uboot.img bs=1024 seek=8 of=$IMAGE status=none conv=notrunc
+rm /mnt/boot/uboot.img
+
+# minor fixes to rootfs
+# TODO: find a better place for this
+rm -f /mnt/etc/resolv.conf
+ln -s ../tmp/resolv.conf /mnt/etc/resolv.conf
+
+# vim: ts=2 sw=2 ft=sh et
