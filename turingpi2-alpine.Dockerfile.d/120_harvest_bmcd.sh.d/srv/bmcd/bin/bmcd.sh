39e665412a5ad893e2aae396d3b75c611836d8c2
abra <abra@tpn4>
2025-06-18 00:21:56 +0000

    turingpi2-alpine: bmcd: tpi msd witk RK1 works

--- turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.sh
+++ turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -eu
 
+# this a quick and dirty hack to run the BMC compiled for buildroot (glibc) on alpine (musl).
+# FIXME: build and run nativly with musl
+
 #set -x
 # this prevents bmcd from binding to /dev/ttyS[1234]
 # this allow ungarbled access with minicom, screen, ...
-MOCK_SERIAL=yes
+MOCK_SERIAL=yes # set it the default as IMHO currently the serial feautre of the BMC is useless
 
 BMCD_CMDLINE=
 while [ $# != 0 ]; do
@@ -16,15 +19,23 @@ while [ $# != 0 ]; do
   shift
 done
 
-# "tinker" environment to run bmcd
+# "tinker" environment to run bmcd using a diffrent mount namespace to move mountpoints around
 exec unshare -m -- sh -eu << EOF
   #set -x
 
   if [ "$MOCK_SERIAL" = yes ]; then
   # prepare /dev
+  #  * move old /dev to /rom/dev
+  #  * mount tmpfs to /rom/tmp
+  #  * overlay upper=/rom/tmp and lower=/rom/dev to /dev
+  #  this way we get mdev/hotplug updates in /dev, besides the tty modifcations below
+  #  this allow tpi msd to work
     mount -o move,rprivate /dev /rom/dev
-    mount -o rprivate -t tmpfs bmcd-dev /dev
-    cp -a /rom/dev/* /dev
+    mount -t tmpfs tmpfs /rom/tmp -o size=256k
+
+    mkdir -p /rom/tmp/upper /rom/tmp/work
+    mount -t overlay -o lowerdir=/rom/dev,upperdir=/rom/tmp/upper,workdir=/rom/tmp/work bmcd-dev-overlay /dev
+
     rm -f /dev/ttyS[1234]
     ln -s tty7 /dev/ttyS1
     ln -s tty7 /dev/ttyS2
