7fe95c10d37735d07d0175aead273889aca8c946
j0ju <j0ju@mac>
2025-06-22 12:26:44 +0200

    turingpi2-buildroot: fix build on Rancher Desktop/OSX (apple silicon)

--- turingpi2-buildroot.Workspace.d/prepare.sh
+++ turingpi2-buildroot.Workspace.d/prepare.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
-set -eu
 umask 022
 PS4="> ${0##*/}: "
 
@@ -17,8 +16,6 @@ cd /workspace
 if [ -d bmc-firmware ]; then
   cd bmc-firmware/buildroot
   make
-  #echo "E: already checked out, clean manually or do make clean-volumes"
-  #exit 1
 else :
   git clone https://github.com/turing-machines/BMC-Firmware.git bmc-firmware
   cd bmc-firmware
@@ -28,17 +25,12 @@ else :
 
   #- prepare /workspace/bmc-firmware/buildroot
   ./scripts/configure.sh
-
-  #- link image output to ./output
-  mkdir -p /src/input/turingpi2-buildroot
-  mkdir -p /workspace/bmc-firmware/buildroot/output
-  [ -z "$OWNER" ] || \
-    chown -R "$OWNER${GROUP:+:$GROUP}" /src/input/turingpi2-buildroot /workspace/bmc-firmware/buildroot/output
-  rm -rf /workspace/bmc-firmware/buildroot/output/images
-  ln -s /src/input/turingpi2-buildroot /workspace/bmc-firmware/buildroot/output/images
-
   ./scripts/build.sh
 fi
+
+mkdir -p /src/input/turingpi2-buildroot
+cp /workspace/bmc-firmware/buildroot/output/images/rootfs.tar                /src/input/turingpi2-buildroot
+cp /workspace/bmc-firmware/buildroot/output/images/u-boot-sunxi-with-spl.bin /src/input/turingpi2-buildroot/uboot.img
 
 # fix permissions if container UID != host UID
 [ -z "$OWNER" ] || \
