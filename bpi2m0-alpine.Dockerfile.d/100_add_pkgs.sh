6b1fa0556b508e5000bd0fb921d61254c9edc85b
j0ju <j0ju@mac>
2025-07-03 12:17:36 +0200

    bpi2m0-alpine: add SBC specific packages

---
+++ bpi2m0-alpine.Dockerfile.d/100_add_pkgs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
+set -eu
+umask 022
+
+PS4='> ${0##*/}: '
+set -x
+
+chroot /target \
+  apk add \
+    wpa_supplicant wpa_supplicant-openrc hostapd hostapd-openrc \
+    cloud-init \
+# EO chroot /target apk add
