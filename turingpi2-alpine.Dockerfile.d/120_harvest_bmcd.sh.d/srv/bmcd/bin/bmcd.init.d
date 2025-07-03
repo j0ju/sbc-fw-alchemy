db467ea8e93736bdc1ffef05c1b229361a569f7b
j0ju <j0ju@mac>
2025-07-03 12:14:08 +0200

    turingpi2-alpine: update config and wrappers for havested bmcd

--- turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.init.d
+++ turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.init.d
@@ -2,6 +2,7 @@
 
 name="${RC_SVCNAME}"
 command="/usr/local/sbin/bmcd"
+supervisor=supervise-daemon
 
 CERTFILE="/etc/ssl/certs/bmcd_cert.pem"
 KEYFILE="/etc/ssl/certs/bmcd_key.pem"
@@ -12,7 +13,7 @@ command_background=true
 pidfile="${RC_PREFIX}/run/bmcd.pid"
 
 depend() {
-  need net otg
+  need net # otg
   use logger
   after firewall
 }
@@ -21,13 +22,4 @@ start_pre() {
   if [ ! -f "$CERTFILE" ] || [ ! -f "$KEYFILE" ]; then
     sh /etc/bmcd/generate_self_signedx509.sh
   fi
-}
-
-start() {
-  rm -f /run/bmcd.pipe
-  mknod /run/bmcd.pipe p
-  "$command" $command_args < /dev/null > /run/bmcd.pipe 2>&1 &
-  pid=$!
-  echo $pid > "$pidfile"
-  /usr/bin/logger -t "bmcd[$pid]" < /run/bmcd.pipe &
 }
--- turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.sh
+++ turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/bmcd.sh
@@ -1,35 +1,32 @@
 #!/bin/sh
 set -eu
 
-# this a quick and dirty hack to run the BMC compiled for buildroot (glibc) on alpine (musl).
-# FIXME: build and run nativly with musl
-
-#set -x
+PS4="> ${0##*/}[$$] > "
+set -x
 # this prevents bmcd from binding to /dev/ttyS[1234]
 # this allow ungarbled access with minicom, screen, ...
-MOCK_SERIAL=yes # set it the default as IMHO currently the serial feautre of the BMC is useless
+MOCK_SERIAL=yes
+STDOUT_TO_SYSLOG=yes
 
 BMCD_CMDLINE=
 while [ $# != 0 ]; do
   case "$1" in
     --no-serial | -S ) MOCK_SERIAL=yes ;;
     --serial    | -s ) MOCK_SERIAL=no ;;
+    --syslog    | -L ) STDOUT_TO_SYSLOG=yes ;;
+    --no-syslog | -l ) STDOUT_TO_SYSLOG=no ;;
     * )                BMCD_CMDLINE="$BMCD_CMDLINE $1"
   esac
   shift
 done
 
-# "tinker" environment to run bmcd using a diffrent mount namespace to move mountpoints around
+# "tinker" environment to run bmcd
 exec unshare -m -- sh -eu << EOF
-  #set -x
+  PS4="> ${0##*/}[\$$] > mountns > "
+  set -x
 
   if [ "$MOCK_SERIAL" = yes ]; then
   # prepare /dev
-  #  * move old /dev to /rom/dev
-  #  * mount tmpfs to /rom/tmp
-  #  * overlay upper=/rom/tmp and lower=/rom/dev to /dev
-  #  this way we get mdev/hotplug updates in /dev, besides the tty modifcations below
-  #  this allow tpi msd to work
     mount -o move,rprivate /dev /rom/dev
     mount -t tmpfs tmpfs /rom/tmp -o size=256k
 
@@ -42,9 +39,20 @@ exec unshare -m -- sh -eu << EOF
     ln -s tty7 /dev/ttyS3
     ln -s tty7 /dev/ttyS4
   fi
+  if [ "$STDOUT_TO_SYSLOG" = yes ]; then
+    rm -f /run/bmcd.pipe
+    mknod /run/bmcd.pipe p
+    if [ "$MOCK_SERIAL" = yes ]; then
+      mount -o bind -r /rom/dev/log /dev/log
+    fi
+    /usr/bin/logger -s -t "bmcd[$$]" < /run/bmcd.pipe &
+    exec > /run/bmcd.pipe
+    rm -f /run/bmcd.pipe
+  fi
 
   # prepare /lib
+  mount -o bind,ro,rprivate /srv/bmcd/bin/bmcd "$0"
   mount -o bind,ro,rprivate /srv/bmcd/lib /lib
 
-  exec /srv/bmcd/bin/bmcd $BMCD_CMDLINE
+  exec "$0" $BMCD_CMDLINE 2>&1
 EOF
