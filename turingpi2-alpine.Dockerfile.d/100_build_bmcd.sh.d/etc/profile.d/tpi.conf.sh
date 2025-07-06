2aa294eae1f0009653983276fe9ea04c33d3f188
j0ju <j0ju@mac>
2025-07-06 23:04:01 +0200

    turingpi2-alpine: migrate to native compiles BMCd (saving approx 20MB of space)

---
+++ turingpi2-alpine.Dockerfile.d/100_build_bmcd.sh.d/etc/profile.d/tpi.conf.sh
@@ -0,0 +1,6 @@
+# this sets defaults for bmcd access via tpi
+# enable and keep in sync with /etc/bmcd/config.yaml
+# if changed
+
+#export TPI_HOST=[::1]
+#export TPI_PORT=443
--- turingpi2-alpine.Dockerfile.d/120_harvest_bmcd.sh.d/srv/bmcd/bin/tpi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-export LD_LIBRARY_PATH=/srv/bmcd/lib 
-exec /srv/bmcd/lib/ld-linux.so.3 /srv/bmcd/bin/tpi "$@"
