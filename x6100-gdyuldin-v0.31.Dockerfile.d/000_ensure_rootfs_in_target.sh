3fb98edfc039c4c9f2fbae032a3b26d0555e38fc
abra <abra@tpn4>
2025-07-10 15:10:07 +0000

    x6100-gdyuldin-v0.31: initial import

---
+++ x6100-gdyuldin-v0.31.Dockerfile.d/000_ensure_rootfs_in_target.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+# (C) 2023-2025 Joerg Jungermann, GPLv2 see LICENSE
+
+PS4='> ${0##*/}: '
+set -x
+
+mv /target /target.old ;\
+mv /target.old/part2 /target ;\
+cp -a /target.old/part1/* /target/boot ;\
+cp /target.old/uboot.egn /target/boot ;\
+rm -rf /target.old ;\
