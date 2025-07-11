b6d9384b7cba820973fa2b98d5db59996a4f478e
abra <abra@tpn4>
2025-07-11 13:09:57 +0000

    armhf-alpine-3.22: initial add of base rootfs and OptAlpine rootfs

---
+++ armhf-alpine-3.22.Dockerfile.d/000_prepare_target.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+# - shell environment file for run-parts scripts in this directory
+# (C) 2024-2025 Joerg Jungermann, GPLv2 see LICENSE
+
+PS4='> ${0##*/}: '
+#set -x
+#
+umask 022
+
+# prepare a local minimal /dev and resolv.conf
+cp -a /dev/* /target/dev
+rm -f /target/etc/resolv.conf
+cp /etc/resolv.conf /target/etc/resolv.conf
