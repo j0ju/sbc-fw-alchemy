067081104f0494cec4405bf0dcd7ac5565215200
abra <abra@tpn4>
2025-07-11 19:50:41 +0000

    turingpi2-alpine: update script to cope with alpine 3.22

--- turingpi2-alpine.Dockerfile.d/100_build_bmcd.sh
+++ turingpi2-alpine.Dockerfile.d/100_build_bmcd.sh
@@ -17,10 +17,10 @@ umask 022
 #- settings
   BMCD_VERSION=v2.3.7
   BMCUI_VERSION=v3.3.6
-  
+
   #TPI_VER=f9a5d58f42428f861693bdeac5acc0171872d807
   TPI_VER=HEAD
- 
+
   PREFIX=/opt/bmcd
 
   BMCD_ARCHIVE="https://github.com/turing-machines/bmcd/archive/refs/tags/${BMCD_VERSION}.tar.gz"
@@ -38,14 +38,14 @@ umask 022
 chroot /build /bin/sh -eu <<EOchroot
   PS4='> ${0##*/}:install bmcdUI: '
   set -x
-  
+
   mkdir -p /src/bmcUI
   cd /src/bmcUI
   wget "$BMCUI_BLOB"
   tar xf "${BMCUI_BLOB##*/}" --owner=0 --group=0 --no-same-owner --no-same-permissions
-  
+
   mv dist www
-  chown -R 0:0 www 
+  chown -R 0:0 www
   find www -type d -exec chown 0755 {} +
   find www -type f -exec chown 0644 {} +
   cp -a www "$PREFIX"/www
@@ -61,7 +61,7 @@ chroot /build /bin/sh -eu <<EOchroot
   git clone "$TPI_GIT" tpi
   cd tpi
   git checkout -b local "$TPI_VER"
-  cargo build --release --features localhost,native-tls 
+  cargo build --release --features localhost,native-tls
 
   mkdir -p "$PREFIX/bin"
   cp target/release/tpi "$PREFIX/bin/tpi.bin"
@@ -121,6 +121,6 @@ find . ! -type d | \
   done
 
 #- link wrappers
-  mkdir -p /usr/local/bin /usr/local/sbin
-  ln -s ../../../opt/bmcd/bin/tpi.wrap /usr/local/bin
-  ln -s ../../../opt/bmcd/bin/bmcd.wrap /usr/local/sbin
+  mkdir -p /target/usr/local/bin /target/usr/local/sbin
+  ln -s ../../../opt/bmcd/bin/tpi.wrap /target/usr/local/bin/tpi
+  ln -s ../../../opt/bmcd/bin/bmcd.wrap /target/usr/local/sbin/bmcd
