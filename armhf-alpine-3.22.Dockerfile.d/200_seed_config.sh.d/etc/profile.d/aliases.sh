583e3556cb481ba0cb724acdd58f99863d33151c
j0ju <j0ju@mac>
2025-08-14 23:39:09 +0200

    armhf-alpine-3.22: add user convinience and support for /etc/rc.local

--- armhf-alpine-3.22.Dockerfile.d/200_seed_config.sh.d/etc/profile.d/aliases.sh
+++ armhf-alpine-3.22.Dockerfile.d/200_seed_config.sh.d/etc/profile.d/aliases.sh
@@ -4,5 +4,5 @@ alias rL="__ETC_PROFILE_READ=no; . /etc/profile"
 alias rS="exec tx"
 alias log="logread -F"
 
-#alias Faux='ps faux | grep [^]]$'
 alias Faux="ps faux | awk '\$5!=0 && \$0 !~ \"awk\"'"
+alias ip="ip -c"
--- armhf-alpine-3.22.Dockerfile.d/200_seed_config.sh.d/root/.profile
+++ armhf-alpine-3.22.Dockerfile.d/200_seed_config.sh.d/root/.profile
@@ -1,4 +1,5 @@
 PS1='\u @ \h$(__PS1_ExitCode)$(__PS1_GitStat) > \w > '
+alias r="exec sudo -"
 
 # start a log that can be interupted with ^C per default
 #case "$(tty)" in
