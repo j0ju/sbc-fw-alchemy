09bb593bf165ad1a6713dd20c4d4c66c93e87953
abra <abra@tpn4>
2025-09-19 09:13:52 +0000

    amd64-EFI-CI-trixie: fix package name

--- amd64-EFI-CI-trixie.Dockerfile.d/200_install_tools.sh
+++ amd64-EFI-CI-trixie.Dockerfile.d/200_install_tools.sh
@@ -10,6 +10,6 @@ chroot /target \
   apt-get install -y \
     mc tmux screen vim-nox tcpdump mtr-tiny strace \
     lsof console-setup cifs-utils nfs-common pv ntfs-3g \
-    ifupdown-ng ifupdown-ng-compat bird fastd \
+    ifupdown-ng ifupdown-ng-compat bird2 fastd \
   #
 #
