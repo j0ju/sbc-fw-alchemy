6cfaaa5e655e957710b6614ac53b23bc7e08cee7
abra <abra@tpn4>
2025-09-19 17:35:50 +0000

    armhf-alpine-3.22: adapt ifupdown-ng executor installation to upstream

--- armhf-alpine-3.22.Dockerfile.d/199_update-ifupdown-ng-executors.sh
+++ armhf-alpine-3.22.Dockerfile.d/199_update-ifupdown-ng-executors.sh
@@ -3,5 +3,5 @@
 set -eu
 
 git clone https://github.com/ifupdown-ng/ifupdown-ng.git /ifupddown-ng-git
-cp /ifupddown-ng-git/executor-scripts/linux/* /target/usr/libexec/ifupdown-ng
+cp /ifupddown-ng-git/executors/linux/* /target/usr/libexec/ifupdown-ng
 chmod 0755 /target/usr/libexec/ifupdown-ng/*
