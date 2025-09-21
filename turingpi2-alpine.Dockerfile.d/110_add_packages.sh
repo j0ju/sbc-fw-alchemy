131baf41f501bbb20ce12c68ec5f7d2d24dd281b
abra <abra@tpn4>
2025-09-21 09:11:37 +0000

    turingpi2-alpine: add iptables to base

---
+++ turingpi2-alpine.Dockerfile.d/110_add_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
+set -eu
+umask 022
+
+#- debug
+  trap 'exit $?' EXIT
+  PS4='> ${0##*/}: '
+  set -x
+
+mkdir -p /target/tmp/cache/apk /target/tmp/cache/etckeeper
+chroot /target apk add iptables iptables-openrc
