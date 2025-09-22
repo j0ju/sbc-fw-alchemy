89eae44def6a2480128e71d0389b92f6ae50cb55
j0ju <j0ju>
2025-09-22 12:30:17 +0000

    img-mangler/amd64-EFI-generate-efi-bootable-iso.sh: now handles images which are build in / (instead of /target)

--- img-mangler/amd64-EFI-generate-efi-bootable-iso.sh
+++ img-mangler/amd64-EFI-generate-efi-bootable-iso.sh
@@ -6,9 +6,12 @@ set -eu
 OUTPUT="$1"
 LIVE_COMMAND_LINE="boot=live toram ip=frommedia"
 
-# get kernel and initrd from rootfs in /target
+DST=""$DST""
+cd "$DST" 2> /dev/null || DST=/
+
+# get kernel and initrd from rootfs in $DST
 get_kernel_files_names() {
-    GRUB_KERNEL_CMDLINE="$( < /target/boot/grub/grub.cfg awk '$1 == "linux" && $0 !~ "single " {print $0}' | sed -re 's/^[[:space:]]+//' | sort -u )"
+    GRUB_KERNEL_CMDLINE="$( < $DST/boot/grub/grub.cfg awk '$1 == "linux" && $0 !~ "single " {print $0}' | sed -re 's/^[[:space:]]+//' | sort -u )"
 
     set -- $GRUB_KERNEL_CMDLINE
     KERNEL="$2"
@@ -22,17 +25,17 @@ get_kernel_files_names() {
         esac
     done
 
-    KERNEL="$( cd /target/ ; ls boot/vmlinuz* | sort -nr | { read k; echo $k; } )"
-    INITRD="$( cd /target/ ; ls boot/initrd*  | sort -nr | { read k; echo $k; } )"
+    KERNEL="$( cd "$DST"; ls boot/vmlinuz* | sort -nr | { read k; echo $k; } )"
+    INITRD="$( cd "$DST"; ls boot/initrd*  | sort -nr | { read k; echo $k; } )"
 }
 get_kernel_files_names
 
 # install live-boot
-chroot /target apt-get clean
-rm -f /target/etc/ssh/ssh_host_*_key* /target/etc/machine-id
+chroot "$DST" apt-get clean
+rm -f "$DST"/etc/ssh/ssh_host_*_key* "$DST"/etc/machine-id
 if ls /prefetch.deb/*.deb > /dev/null 2>&1; then
-    mv /prefetch.deb/*.deb /target/var/cache/apt/archives
-    chroot /target sh -euc '
+    mv /prefetch.deb/*.deb "$DST"/var/cache/apt/archives
+    chroot "$DST" sh -euc '
         cd /var/cache/apt/archives
         dpkg -i *.deb
         rm -f *.deb
@@ -40,10 +43,10 @@ if ls /prefetch.deb/*.deb > /dev/null 2>&1; then
     '
 fi
 rm -f \
-    /target/var/lib/apt/lists/*.* \
-    /target/var/lib/apt/lists/partial/* \
-    /target/var/cache/apt/archives/*.* \
-    /target/var/cache/apt/archives/partial/*.* \
+    "$DST"/var/lib/apt/lists/*.* \
+    "$DST"/var/lib/apt/lists/partial/* \
+    "$DST"/var/cache/apt/archives/*.* \
+    "$DST"/var/cache/apt/archives/partial/*.* \
 # EO rm -f
 
 # prepare /iso
@@ -51,19 +54,22 @@ rm -rf /iso
 mkdir -p /iso /iso/boot/grub /iso/EFI/BOOT /iso/live
 
 # place kernel and initrd
-cp /target/$KERNEL /iso/boot/vmlinuz
-cp /target/$INITRD /iso/boot/initrd.img
+cp "$DST"/$KERNEL /iso/boot/vmlinuz
+cp "$DST"/$INITRD /iso/boot/initrd.img
 
 # generate squashfs
-echo "# empty" > /target/etc/fstab
-mksquashfs /target /iso/live/live.squashfs -comp zstd -b 1024k
+echo "# empty" > "$DST"/etc/fstab
+mksquashfs "$DST" /iso/live/live.squashfs -comp xz -b 1024k -one-file-system -ef /dev/fd/0 <<EO_MKSQUASHFS
+/busybox.static
+/iso
+EO_MKSQUASHFS
 
 # generate grub
 cat > /iso/boot/grub/grub.cfg <<EOF
 set timeout=0
 set root=(cd0)
 set prefix=(memdisk)/boot/grub
- 
+
 menuentry 'Linux' {
    echo 'Loading kernel...'
    linux /boot/vmlinuz $KERNEL_COMMAND_LINE $LIVE_COMMAND_LINE
@@ -75,10 +81,10 @@ EOF
 
 grub-mknetdir --net-directory=/boot --subdir=/grub -d /usr/lib/grub/x86_64-efi \
     --modules="\
-        efi_gop efi_uga efifwsetup efinet linuxefi lsefi lsefimmap lsefisystab \
+        efi_gop efi_uga efifwsetup efinet lsefi lsefimmap lsefisystab \
         normal extcmd crypto gettext terminal gzio gcry_crc regexp tftp http ext2 \
         fshelp fat part_msdos part_gpt configfile linux relocator mmap video \
-        linuxefi reboot serial terminfo test efi_gop video_fb efi_uga video_bochs \
+        reboot serial terminfo test efi_gop video_fb efi_uga video_bochs \
         video_cirrus echo loadenv disk search search_fs_uuid search_fs_file \
         search_label zfs xfs ufs2 ufs1_be ufs1 udf tar archelp squash4 xzio lzopio \
         sfs romfs reiserfs procfs odc ntfs nilfs2 newc minix_be minix3_be minix3 \
@@ -88,10 +94,10 @@ grub-mknetdir --net-directory=/boot --subdir=/grub -d /usr/lib/grub/x86_64-efi \
 
 grub-mkstandalone -o /iso/EFI/BOOT/BOOTX64.EFI -O x86_64-efi \
     --modules="\
-        efi_gop efi_uga efifwsetup linuxefi lsefi lsefimmap lsefisystab \
+        efi_gop efi_uga efifwsetup lsefi lsefimmap lsefisystab \
         normal extcmd crypto gettext terminal gzio gcry_crc regexp tftp http ext2 \
         fshelp fat part_msdos part_gpt configfile linux relocator mmap video \
-        linuxefi reboot serial terminfo test efi_gop video_fb efi_uga video_bochs \
+        reboot serial terminfo test efi_gop video_fb efi_uga video_bochs \
         video_cirrus echo loadenv disk search search_fs_uuid search_fs_file \
         search_label zfs xfs ufs2 ufs1_be ufs1 udf tar archelp squash4 xzio lzopio \
         sfs romfs reiserfs procfs odc ntfs nilfs2 newc minix_be minix3_be minix3 \
@@ -110,8 +116,9 @@ mmd -i /iso/EFI/BOOT/efiboot.img ::EFI
 mmd -i /iso/EFI/BOOT/efiboot.img ::EFI/BOOT
 mcopy -i /iso/EFI/BOOT/efiboot.img /iso/EFI/BOOT/BOOTX64.EFI ::EFI/BOOT/BOOTX64.EFI
 
-# generate ISO 
-xorriso -as mkisofs -V 'EFI_ISO_BOOT' -e EFI/BOOT/efiboot.img -no-emul-boot -o "$OUTPUT" /iso/
+# generate ISO
+set -x
+xorriso -as mkisofs -V 'EFI_ISO_BOOT' -e EFI/BOOT/efiboot.img -no-emul-boot -o /src/"$OUTPUT" /iso/
 
 [ -z "$OWNER" ] || \
-  chown "$OWNER${GROUP:+:$GROUP}" "$OUTPUT"
+  chown "$OWNER${GROUP:+:$GROUP}" /src/"$OUTPUT"
