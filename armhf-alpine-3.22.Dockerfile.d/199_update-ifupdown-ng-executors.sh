01e2448429f8deee76161e36e575247d4e52278a
j0ju <j0ju@mac>
2025-09-25 18:22:41 +0200

    armhf-alpine-3.22: patch ifupdown-ng executor for bridges to allow ports reconfigured without bringing the whole bridge down

--- armhf-alpine-3.22.Dockerfile.d/199_update-ifupdown-ng-executors.sh
+++ armhf-alpine-3.22.Dockerfile.d/199_update-ifupdown-ng-executors.sh
@@ -5,3 +5,33 @@ set -eu
 git clone https://github.com/ifupdown-ng/ifupdown-ng.git /ifupddown-ng-git
 cp /ifupddown-ng-git/executors/linux/* /target/usr/libexec/ifupdown-ng
 chmod 0755 /target/usr/libexec/ifupdown-ng/*
+
+# remove duplicate executors
+rm -f /target/etc/network/*.d/bridge
+
+# copy over config seed
+DST="${DST:-/target}"
+FSDIR="$0.d"
+
+cd "$FSDIR"
+find . ! -type d | \
+  while read f; do
+    f="${f#./}"
+    mkdir -p "${DST}/${f%/*}"
+    case "$f" in
+      */.placeholder ) continue ;;
+    esac
+
+    rm -f "${DST}/$f"
+    chmod 0755 "${DST}/${f%/*}"
+
+    mv "$f" "${DST}/$f"
+    if [ ! -L "${DST}/$f" ]; then
+      if [ -x "${DST}/$f" ]; then
+        chmod 0755 "${DST}/$f"
+      else
+        chmod 0644 "${DST}/$f"
+      fi
+    fi
+    echo " * /$f"
+  done
