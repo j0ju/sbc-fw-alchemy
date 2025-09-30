9eb67b4fd3505c927cdd633e18f1a3d7c8a785d4
j0ju <j0ju@mac>
2025-09-30 18:32:32 +0200

    bpi2m0-alpine: gen-sqfs.sh now adds /boot/build.meta with version information

--- bpi2m0-alpine.Dockerfile.d/gen-sqfs.sh
+++ bpi2m0-alpine.Dockerfile.d/gen-sqfs.sh
@@ -10,9 +10,18 @@ BLOCKSIZE="${BLOCKSIZE:-1024k}"  # 256k 512k
 rm -f /target/etc/resolv.conf
 rm -rf /target/etc/.git
 ln -s ../run/resolv.conf /target/etc/resolv.conf
+ 
+GITREV="$( cd /src ; git log HEAD^..HEAD --oneline | awk '$0=$1' )"
+DATE="$( date +%Y-%m-%d-%H:%M )"
+VERSION=$DATE-$GITREV+dirty
+git status --short | grep -q ^ || \
+  VERSION="${VERSION%+dirty}"
+
+echo "$VERSION" > /target/boot/build.meta
 
 rm -f "$IMAGE"
 mksquashfs /target "$IMAGE" -b "$BLOCKSIZE" -comp "$COMPRESSOR" -quiet
+
 
 [ -z "$OWNER" ] || \
   chown "$OWNER${GROUP:+:$GROUP}" "$IMAGE"
