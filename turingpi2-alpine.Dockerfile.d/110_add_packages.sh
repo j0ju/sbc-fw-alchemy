a744918448288e398755f06bc652edcc20369921
j0ju <j0ju@mac>
2025-09-30 13:06:29 +0200

    turingpi2-alpine: do not remove git and etckeeper anymore, just gets 1M

--- turingpi2-alpine.Dockerfile.d/110_add_packages.sh
+++ turingpi2-alpine.Dockerfile.d/110_add_packages.sh
@@ -9,4 +9,5 @@ umask 022
   set -x
 
 mkdir -p /target/tmp/cache/apk /target/tmp/cache/etckeeper
+#yes | chroot /target apk del git etckeeper
 chroot /target apk add iptables iptables-openrc mtd-utils rdnssd
