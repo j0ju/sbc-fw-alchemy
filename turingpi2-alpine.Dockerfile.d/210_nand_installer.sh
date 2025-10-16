26fdca7258f4cf20f20f5c916f933f01359e1db1
j0ju <j0ju@mac>
2025-10-16 23:56:22 +0200

    turingpi2-alpine: add script to install current running SD image to NAND

---
+++ turingpi2-alpine.Dockerfile.d/210_nand_installer.sh
@@ -0,0 +1,31 @@
+
+#!/bin/sh
+# (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
+set -eu
+umask 022
+
+#- copy over config seed - seed addtional files
+DST="${DST:-/target}"
+FSDIR="$0.d"
+cd "$FSDIR"
+find . ! -type d | \
+  while read f; do
+    f="${f#./}"
+    mkdir -p "${DST}/${f%/*}"
+    case "$f" in
+      */.placeholder ) continue ;;
+    esac
+
+    rm -f "${DST}/$f"
+    chmod 0755 "${DST}/${f%/*}"
+
+    mv "$f" "${DST}/$f"
+    if [ ! -L "${DST}/$f" ]; then
+      if [ -x "${DST}/$f" ]; then
+        chmod 0755 "${DST}/$f"
+      else
+        chmod 0644 "${DST}/$f"
+      fi
+    fi
+    echo " * /$f"
+  done
