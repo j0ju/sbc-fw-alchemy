4be188d7fc2f016f940c372ca2337e42a854d324
abra <abra@tpn4>
2025-11-01 23:26:00 +0000

    rk1-alpine: add podman

---
+++ rk1-alpine.Dockerfile.d/100_add_podman.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# (C) 2025 Joerg Jungermann, GPLv2 see LICENSE
+set -eu
+umask 022
+
+PS4='> ${0##*/}: '
+#set -x
+
+mkdir -p /target/tmp/cache/apk /target/tmp/cache/etckeeper /target/tmp/cache/vim
+
+chroot /target \
+  apk add \
+    podman \
+# EO chroot /target apk add
+
+rm -f /target/etc/.git/HEAD.lock
