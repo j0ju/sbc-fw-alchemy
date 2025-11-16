d64a8b21aa383d1820c2fa3f0e791708026d2193
abra <abra@tpn4>
2025-11-16 19:00:07 +0000

    x6100-v1.1.9-mod: add alternative basebands

---
+++ images/x6100-v1.1.9-mod.Dockerfile.d/110_add_patched_baseband.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+umask 022
+wget https://github.com/gdyuldin/x6100_base_patch/raw/refs/heads/1.1.6/firmwares/X6100_BBFW_V1.1.6_230307001_r7.xgf  -O /target/usr/firmware/X6100_BBFW_V1.1.6_230307001_r7.xgf
+
