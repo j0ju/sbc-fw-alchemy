61c6c083a740ca40bd732b0cd5c95f838cf12b24
abra <abra@tpn4>
2025-11-16 18:59:13 +0000

    lib/img-mangler/unzip-img.sh: unzip has issue with archives from different code pages, implement lazy fallback

--- lib/img-mangler/unzip-img.sh
+++ lib/img-mangler/unzip-img.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 # (C) 2023-2025 Joerg Jungermann, GPLv2 see LICENSE
 set -e
+set -x
 
 IN="$1"
 OUT="$2"
 
-IMG="$(unzip -l "$IN" | grep -o -E "[^ ]+[.]img")"
+IMG="$(unzip -l "$IN" | grep -o -E "[^[:space:]]+[.]img")"
 
-unzip -p "$IN" "$IMG" > "$OUT"
+if ! unzip -p "$IN" "$IMG" > "$OUT"; then
+  if ! unzip -p "$IN" "*.img" > "$OUT"; then
+    echo "E: issue extracting the image '$IMG' from '$IN'."
+    rm -f "$OUT"
+    exit 1
+  fi
+fi
 
 [ -z "$OWNER" ] || \
   chown "$OWNER${GROUP:+:$GROUP}" "$OUT"
