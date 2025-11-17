3e33983858664412b7c31883b54b5b574b8a090d
abra <abra@tpn4>
2025-11-17 20:43:17 +0000

    lib/img-mangler/unzip-img.sh: disable `set -x` & add proper error messages

--- lib/img-mangler/unzip-img.sh
+++ lib/img-mangler/unzip-img.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # (C) 2023-2025 Joerg Jungermann, GPLv2 see LICENSE
 set -e
-set -x
 
 IN="$1"
 OUT="$2"
@@ -10,10 +9,17 @@ IMG="$(unzip -l "$IN" | grep -o -E "[^[:space:]]+[.]img")"
 
 if ! unzip -p "$IN" "$IMG" > "$OUT"; then
   if ! unzip -p "$IN" "*.img" > "$OUT"; then
-    echo "E: issue extracting the image '$IMG' from '$IN'."
+    # FIXME: this only works if there is an ".img" and only one in the .zip
+    echo "E: issue extracting the image '$IMG' from '$IN'." >&2
     rm -f "$OUT"
     exit 1
   fi
+fi
+
+if [ ! -s "$OUT" ]; then
+  echo "E: issue extracting the image '$IMG' from '$IN'." >&2
+  rm -f "$OUT"
+  exit 1
 fi
 
 [ -z "$OWNER" ] || \
