e1af412dfe7ede44e98f406bc37f1b6a720fd44d
abra <abra@tpn4>
2025-11-29 20:22:20 +0000

    recipes/alpine/SBC-base: add aliases for mc to profile, improve editor selection

--- recipes/alpine/SBC-base/200_SBC-base.seed-config.sh.d/etc/profile.d/EDITOR.sh
+++ recipes/alpine/SBC-base/200_SBC-base.seed-config.sh.d/etc/profile.d/EDITOR.sh
@@ -1,13 +1,18 @@
 # - /etc/profile.d/EDITOR.sh -
-case "$(tty 2>/dev/null):$(which vim 2>/dev/null)" in
-  /dev/ttyS*:* | /dev/ttyGS*:* | *: )
-    export EDITOR="/bin/busybox vi"
-    export VISUAL="/bin/busybox vi"
-    alias vi="/bin/busybox vi"
-    ;;
-  *:/* )
+
+case "$TERM:$(tty 2>/dev/null):$(which vim 2>/dev/null)" in
+  screen:*:/* | \
+  xterm*:*:/* | \
+  linux:*:/*  )
     export EDITOR="$(which vim)"
     export VISUAL="$(which vim)"
     alias vi="$(which vim)"
+    ;;
+  vt100:*              | \
+  *:/dev/tty[A-Z]*:*   | \
+  *:                   )
+    export EDITOR="/bin/busybox vi"
+    export VISUAL="/bin/busybox vi"
+    alias vi="/bin/busybox vi"
     ;;
 esac
--- recipes/alpine/SBC-base/200_SBC-base.seed-config.sh.d/etc/profile.d/aliases.sh
+++ recipes/alpine/SBC-base/200_SBC-base.seed-config.sh.d/etc/profile.d/aliases.sh
@@ -6,7 +6,14 @@ alias log="logread -F"
 
 alias Faux="ps faux | awk '\$5!=0 && \$0 !~ \"awk\"'"
 alias ip="ip -c"
+alias mc="mc -adX"
 
+mcd() {
+  __mcd_tmp="${TMPDIR:-/tmp}/.mcd$(tr -dc "a-zA-Z0-9.,:+-" < /dev/urandom | head -c32)"
+  mc -P "$__mcd_tmp" "$@"
+  cd "$( cat "$__mcd_tmp" 2> /dev/null && rm -f "$__mcd_tmp" )" 2> /dev/null
+  unset __mcd_tmp
+}
 dSH() {
   local f="$1"
   if which "$f"; then
