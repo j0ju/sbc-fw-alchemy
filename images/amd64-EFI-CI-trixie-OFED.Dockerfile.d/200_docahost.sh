2dfe3120acaf9fb351fb804735b3694ac806da93
J0erg Jungermann <jj@borkum.net>
2026-04-16 22:02:55 +0000

    images/amd64-EFI-CI-trixie-OFED: use pre-downloaded artifact from ./input

--- images/amd64-EFI-CI-trixie-OFED.Dockerfile.d/200_docahost.sh
+++ images/amd64-EFI-CI-trixie-OFED.Dockerfile.d/200_docahost.sh
@@ -9,13 +9,15 @@ set -x
 PKG="https://www.mellanox.com/downloads/DOCA/DOCA_v3.2.2/host/doca-host_3.2.2-035000-25.10-debian13_amd64.deb"
 
 cd "$DST"
-wget "$PKG"
+#wget "$PKG"
 PKG="${PKG##*/}"
 
 sudo dpkg -i "$PKG"
 
 sudo apt-get update
-sudo apt-get -y install doca-networking
+#sudo apt-get -y install doca-runtime
+sudo apt-get -y install doca-basic rshim
+#sudo apt-get -y install doca-networking
 
 # add some benchmarking tools
 export DEBIAN_FRONTEND=noninteractive
