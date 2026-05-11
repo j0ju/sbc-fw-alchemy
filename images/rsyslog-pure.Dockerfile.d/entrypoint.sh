ea4ad296ff6cc2dacb2c4db4c9a35373fbb2bd70
J0erg Jungermann <jj@borkum.net>
2026-05-11 21:33:02 +0000

    images/rsyslog-pure: improve config

--- images/rsyslog-pure.Dockerfile.d/entrypoint.sh
+++ images/rsyslog-pure.Dockerfile.d/entrypoint.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 
 set -eu
-PS4="${0##*/}: "
+PS4="${0##*/}[$$]: "
+
+if [ "${TRACE:-}" = yes ]; then
+  set -x
+fi
 
 if [ "${1:-}" = "-" ]; then
   set -x
   /bin/busybox --install -s /bin
   exec /bin/ash -
 elif [ -n "${1:-}" ]; then
-  set -x
   "$@"
   exit $?
 fi
 
-if [ -n "$UNIFIED_RSYSLOG_REMOTE" ]; then
+if [ -n "${UNIFIED_RSYSLOG_REMOTE:-}" ]; then
 
-  # disable stdout in rsyslog config
-  sed -i -r \
-      -e '/proc[\/]self[\/]fd/ s/^/#/' \
-  /etc/rsyslog.conf
+  if ! [ "${KEEP_STDOUT:-}" = yes ]; then
+    # disable stdout in rsyslog config
+    /bin/busybox sed -i -r \
+        -e '/proc[\/]self[\/]fd/ s/^/#/' \
+      /etc/rsyslog.conf
+  fi
 
   #   templating if UNIFIED_RSYSLOG_REMOTE is set
   #       enable tcpforwarding queued
-  sed -i -r \
+  /bin/busybox sed -i -r \
       -e '/"omfwd"/ s|^#||' \
       -e '/"omfwd"/ s|[$]UNIFIED_RSYSLOG_REMOTE|'"$UNIFIED_RSYSLOG_REMOTE"'|' \
     /etc/rsyslog.conf
@@ -31,8 +36,8 @@ fi
 # Q: other options to add via environment
 
 # small thumbscrews
-/bin/busybox rm -f /bin/sh # remove shell
+#/bin/busybox rm -f /bin/sh # remove shell
 export PATH=
 
-set -x
+#set -x
 exec /usr/sbin/rsyslogd -n
