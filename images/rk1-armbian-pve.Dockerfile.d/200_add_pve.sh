6c3ca814f86080fab8066ab9ea11f032f470abec
Joerg Jungermann <jj@borkum.net>
2026-08-20 20:18:47 +0000

    images/rk1-armbian-pve: etckeeper: ignore /etc/pve, disable /etc/issue generation

--- images/rk1-armbian-pve.Dockerfile.d/200_add_pve.sh
+++ images/rk1-armbian-pve.Dockerfile.d/200_add_pve.sh
@@ -5,6 +5,11 @@ set -eu
 . "$SRC/lib.sh"; init
 #set -x
 
+# ignore clustered files from pve
+cat >> "$DST"/etc/.gitignore <<EOF
+pve
+EOF
+
 # add proxmox repository
 wget https://enterprise.proxmox.com/debian/proxmox-archive-keyring-trixie.gpg -O $DST/usr/share/keyrings/proxmox-archive-keyring.gpg
 cat > $DST/etc/apt/sources.list.d/pve-install-repo.sources << EOL
@@ -16,22 +21,23 @@ Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 EOL
 
 # fetch needed updates
-chroot $DST apt-get update
-chroot $DST apt-get full-upgrade -y
+chroot "$DST" apt-get update
+chroot "$DST" apt-get full-upgrade -y
 
 #- fix missing ifupdown2 version
 wget http://download.proxmox.com/debian/dists/trixie/pve-no-subscription/binary-amd64/ifupdown2_3.3.0-1%2Bpmx12_all.deb \
-  -O $DST/ifupdown_all.deb
+  -O "$DST/ifupdown_all.deb"
 if ! chroot "$DST" dpkg -i /ifupdown_all.deb; then
   # deal with missing depencies
   chroot "$DST" apt-get install -f -y
 fi
-rm -f $DST/ifupdown_all.deb
+rm -f "$DST/ifupdown_all.deb"
 
 # fake container env
-> $DST/.dockerenv
-chroot $DST apt-get install -y --no-install-recommends proxmox-ve
-rm -f $DST/.dockerenv
+> "$DST/.dockerenv"
+chroot "$DST" apt-get install -y --no-install-recommends proxmox-ve
+rm -f "$DST/.dockerenv"
 
 #- disable enterprise apt sources until we have a subscription
-echo 'Enabled: no' >> $DST/etc/apt/sources.list.d/pve-enterprise.sources
+echo 'Enabled: no' >> "$DST/etc/apt/sources.list.d/pve-enterprise.sources"
+chroot "$DST" systemctl disable pvebanner.service
